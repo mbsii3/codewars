8c1ea3c0d46565b2ac48a13db765b636d62ec0e0
mbsii3 <mshortjr3@gmail.com>
2023-06-14 14:31:01 -0500

    add hoop challenge solution

--- 8kyu/keep-the-hoop.js
+++ 8kyu/keep-the-hoop.js
@@ -6,3 +6,11 @@
 // If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 
 // My solution
+
+function hoopCount (n) {
+   if (n >= 10) {
+    return "Great, now move on to tricks"
+   } else {
+    return "Keep at it until you get it"
+   }
+}

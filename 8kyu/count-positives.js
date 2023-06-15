65718836c5bf7351cc1b3afd57688ff7efbc2212
mbsii3 <mshortjr3@gmail.com>
2023-06-15 11:26:57 -0500

    add my  count positive challenge solution

--- 8kyu/count-positives.js
+++ 8kyu/count-positives.js
@@ -8,3 +8,14 @@
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65]
 
 // My solution
+
+function countPositivesSumNegatives(input) {
+  let positiveNums = 0;
+    let negativeNums = 0;
+    if (input === null || input.length === 0) {
+      return [];
+    } else {
+      input.forEach((num) => num > 0 ? positiveNums++ : negativeNums += num);
+    }
+    return [positiveNums , negativeNums];
+}
